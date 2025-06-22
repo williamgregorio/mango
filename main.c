7769600e5e31ad8d87bdfe6e2547b4b9ff0c9070
William <mrwilliamgregorio@yahoo.com>
2025-06-22 01:20:01 -0400

    Assign struct to raw make copy before changes.

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ void enableRawMode() {
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
 
-  struct termios raw;
+  struct termios raw = orig_termios;
   raw.c_lflag &= ~(ECHO | ICANON);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
