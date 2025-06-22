b9e9c9648ce0ec3e27e8ed68b436cb4e18b384c6
William <mrwilliamgregorio@yahoo.com>
2025-06-21 22:51:57 -0400

    fresh canvas.

--- main.c
+++ main.c
@@ -1,15 +1,4 @@
-// sessions are controlled per projects
-// a project has a name with a log of history good or bad
-// a user can move along different projects as he chooses but only one per view
-// a project should save a struct of valued data that has identity of completion
-// writing to a database that is quick is essential, sqlite3 is of choice.
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-int main(void) {
-  char cwd[PATH_MAX];
-
+int main() {
   return 0;
 }
